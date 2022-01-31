21aa2111495b25878bbea717abda488e3334003b
dileep <manchikantidileep@gmail.com>
2022-01-31 21:59:46 +0530

    error fixed

--- solutions/assignment1-solution/app.js
+++ solutions/assignment1-solution/app.js
@@ -3,19 +3,21 @@
 
 angular.module('LunchCheck', [])
 
-.controller('LunchCheckController',verifyLunch);
-verifyLunch.$inject=["$scope"];
- var verifyLunch=function($scope){
+.controller('LunchCheckController',function($scope){
 	$scope.list="";
 	$scope.isBalanceDiet=function(){
 		var items=$scope.list.split(",");
-		if(items.length==0)$scope.result="Plese enter Data first.."
-		if(items.length<=3){
+		if(items.length==1 && items[0]==""){
+			$scope.result="Please enter Data first..";
+		}
+		else if(items.length<=3){
 			$scope.result="the diet list is with in limit. Enjoy Eating!!";
 		}
 		else{
 			$scope.result="the diet list is not with in limit. It's too much heavy!!!"
 		}
 	}
-}
+});
+// verifyLunch.$inject=["$scope"];
+//  var verifyLunch=
 })();
