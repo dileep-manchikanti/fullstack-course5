6cc4ea9da03f6afef4b2c831072847148c24ed14
dileep <manchikantidileep@gmail.com>
2022-02-13 01:22:37 +0530

    assignment4

--- examples/Lecture41/src/shoppinglist/shoppinglist.service.js
+++ examples/Lecture41/src/shoppinglist/shoppinglist.service.js
@@ -13,21 +13,21 @@ function MenuService($q, $timeout) {
   var items = [];
 
   // Pre-populate a no cookie list
-  // items.push({
-  //   name: "Sugar",
-  //   quantity: "2 bags",
-  //   description: "Sugar used for baking delicious umm... baked goods."
-  // });
-  // items.push({
-  //   name: "flour",
-  //   quantity: "1 bags",
-  //   description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
-  // });
-  // items.push({
-  //   name: "Chocolate Chips",
-  //   quantity: "3 bags",
-  //   description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
-  // });
+  items.push({
+    name: "Sugar",
+    quantity: "2 bags",
+    description: "Sugar used for baking delicious umm... baked goods."
+  });
+  items.push({
+    name: "flour",
+    quantity: "1 bags",
+    description: "High quality wheat flour. Mix it with water, sugar, 2 raw eggs."
+  });
+  items.push({
+    name: "Chocolate Chips",
+    quantity: "3 bags",
+    description: "Put these in the dough. No reason, really. Gotta store them somewhere!"
+  });
 
   // Simulates call to server
   // Returns a promise, NOT items array directly
@@ -35,17 +35,21 @@ function MenuService($q, $timeout) {
     var deferred = $q.defer();
 
     // Wait 2 seconds before returning
+    $timeout(function () {
+      // deferred.reject(items);
+      deferred.resolve(items);
+    }, 800);
 
-    var response=$http({
-      method:"GET",
-      url:'https://davids-restaurant.herokuapp.com/categories.json'
-    });
-    console.log("hi");
-    response.then(function(info){
-      console.log(info.data);
-      deferred.resolve(info.data);
-    });
-    console.log(response);
+    // var response=$http({
+    //   method:"GET",
+    //   url:'https://davids-restaurant.herokuapp.com/categories.json'
+    // });
+    // console.log("hi");
+    // response.then(function(info){
+    //   console.log(info.data);
+    //   deferred.resolve(info.data);
+    // });
+
     return deferred.promise;
   };
 }
