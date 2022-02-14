5d5c9e22d5b76c294e940c3f01d625b21f5cb0de
dileep <manchikantidileep@gmail.com>
2022-02-14 08:49:53 +0530

    Assignemnt4

--- examples/Lecture41/src/shoppinglist/main-shoppinglist.controller.js
+++ examples/Lecture41/src/shoppinglist/main-shoppinglist.controller.js
@@ -13,6 +13,10 @@ function categoryController(MenuService) {
   promise
   .then(function(response){
   	mainlist.items=response.data;
+  	data=response.data;
+    for(var i=0;i<data.length;i++){
+      items.push(data[i]);
+    }
   	console.log(response.data);
   	console.log(mainlist.items);
   });
