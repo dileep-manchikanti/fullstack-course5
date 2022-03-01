1d29e9c17fbea04ab7a65a7ff1143169d7929874
dileep <manchikantidileep@gmail.com>
2022-03-01 09:05:46 +0530

    assignment5

---
+++ solutions/assignment5-solution/src/public/menu-items/menu-items.controller.js
@@ -0,0 +1,17 @@
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('MenuItemsController', MenuItemsController);
+
+MenuItemsController.$inject = ['menuItems'];
+function MenuItemsController(menuItems) {
+  var $ctrl = this;
+  $ctrl.menuItems = menuItems;
+  console.log($ctrl.menuItems);
+  for(var item in $ctrl.menuItems.menu_items){
+  	console.log(item);
+  }
+}
+
+})();
---
+++ solutions/assignment5-solution/src/public/public.routes.js
@@ -0,0 +1,59 @@
+(function() {
+'use strict';
+
+angular.module('public')
+.config(routeConfig);
+
+/**
+ * Configures the routes and views
+ */
+routeConfig.$inject = ['$stateProvider','$urlRouterProvider'];
+function routeConfig ($stateProvider,$urlRouterProvider) {
+  // Routes
+  // $urlRouterProvider.otherwise('public.home');
+  $stateProvider
+    .state('public', {
+      abstract: true,
+      templateUrl: 'src/public/public.html'
+    })
+    .state('public.home', {
+      url: '/',
+      templateUrl: 'src/public/home/home.html'
+    })
+    .state('public.menu', {
+      url: '/menu',
+      templateUrl: 'src/public/menu/menu.html',
+      controller: 'MenuController',
+      controllerAs: 'menuCtrl',
+      resolve: {
+        menuCategories: ['MenuService', function (MenuService) {
+          return MenuService.getCategories();
+        }]
+      }
+    })
+    .state('public.menuitems', {
+      url: '/menu/{category}',
+      templateUrl: 'src/public/menu-items/menu-items.html',
+      controller: 'MenuItemsController',
+      controllerAs: 'menuItemsCtrl',
+      resolve: {
+        menuItems: ['$stateParams','MenuService', function ($stateParams, MenuService) {
+          return MenuService.getMenuItems($stateParams.category);
+        }]
+      }
+    })
+    .state('public.myInfo',{
+      url: '/user/info',
+      templateUrl: 'src/public/user/user-info.html',
+      controller: 'userInfoCtrl',
+      controllerAs: 'userInfoCtrl'
+    })
+    .state('public.signUp',{
+      url:'/user/signUp',
+      templateUrl: 'src/public/user/signUp.html',
+      controller: 'signUpCtrl',
+      controllerAs:'signUpCtrl'
+    })
+    ;
+}
+})();
