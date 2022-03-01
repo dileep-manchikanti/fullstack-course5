c84aed26afd5f7bd29b1e9aa0593917b9e2b8e6f
dileep <manchikantidileep@gmail.com>
2022-03-01 22:51:51 +0530

    assignment5

--- examples/Lecture59/src/public/user/signUpCtrl.js
+++ examples/Lecture59/src/public/user/signUpCtrl.js
@@ -11,8 +11,12 @@
 			$http.get('https://davids-restaurant.herokuapp.com/categories.json').then(function (response) {
       console.log(signUp.user.favourite);
       console.log(response.data);
-      signUp.user.favourite=response.data[signUp.user.favourite];
-      console.log(signUp.user.favourite);
+      var data=response.data;
+      var favourite=signUp.user.favourite;
+      signUp.user.favourite=undefined;
+      for(var i=0;i<data.length;i++){
+      	if(data[i].short_name==favourite)signUp.user.favourite=data[i];
+      }
       service.user=signUp.user;
       window.alert("Your Info is saved Sucessfully....");
     })
