52bf314f4ed8f6e305fbc762799752c433a90e80
dileep <manchikantidileep@gmail.com>
2022-03-02 10:26:33 +0530

    assignment5

--- examples/Lecture59/src/public/menu-items/menu-items.controller.js
+++ examples/Lecture59/src/public/menu-items/menu-items.controller.js
@@ -8,6 +8,9 @@ MenuItemsController.$inject = ['menuItems'];
 function MenuItemsController(menuItems) {
   var $ctrl = this;
   var items = menuItems;
+  $ctrl.categoryName=items.category.name;
+  $ctrl.short_name=items.category.short_name;
+  $ctrl.special_instructions=items.category.special_instructions;
   $ctrl.menuItems=[];
   for(var i=0;i<items.menu_items.length;i++){
   	$ctrl.menuItems.push(items.menu_items[i]);
