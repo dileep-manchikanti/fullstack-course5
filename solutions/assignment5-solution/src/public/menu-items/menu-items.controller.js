701045a28b2949a1ab9c13360d09b389b0007154
dileep <manchikantidileep@gmail.com>
2022-03-10 01:26:32 +0530

    assignment5

--- solutions/assignment5-solution/src/public/menu-items/menu-items.controller.js
+++ solutions/assignment5-solution/src/public/menu-items/menu-items.controller.js
@@ -12,8 +12,8 @@ function MenuItemsController(menuItems) {
   $ctrl.short_name=items.category.short_name;
   $ctrl.special_instructions=items.category.special_instructions;
   $ctrl.menuItems=items.menu_items;
-  $ctrl.menuItems=items.menu_items[0];
-  console.log(items.menu_items);
+  $ctrl.menuItems=items.menu_items;
+  console.log(items.menu_items[0]);
   for(var item in $ctrl.menuItems){
   	console.log(item);
   }
